df842244041372824ffbd16450f134928d5abb76
edolilloYahoo <edolillo@ymail.com>
2022-04-19 15:52:57 -0400

    test1

--- DSL/javamavenDSL3.groovy
+++ DSL/javamavenDSL3.groovy
@@ -50,7 +50,7 @@ job('Job test Hola Mundo') {
 	description('Aplicacion Hola Mundo de Prueba')
 	steps {
 		shell('''
-			echo "Hola Eduardo Lillo puyol!!!"
+			echo "Hola Eduardo Lillo puyol!!!!!!!!!!!!!!!!!!!!!!!!!"
 		''')
 	}
 }
